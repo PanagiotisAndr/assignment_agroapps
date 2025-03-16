029a64731ac548aee360f7d0b9bec7d7a42bd601
Takis Andreou <takandre2@gmail.com>
2025-03-16 11:01:15 +0200

    main view

---
+++ src/views/ForecastView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import WeatherChart from '../components/weather/Chart.vue'
+</script>
+
+<template>
+  <div class="main-container bg-gray-200 p-10">
+    <div class="bg-white rounded-lg shadow-md">
+      <div class="divider"></div>
+      <WeatherChart />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main-container {
+  background-color: #fcfcfc;
+  .divider {
+    background-color: #eaebef;
+    padding: 20px 0;
+  }
+}
+</style>
